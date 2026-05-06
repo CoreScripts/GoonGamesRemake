c60b98ef7273e3661e5f8ffa529f9aea77f3b7cc
CoreScripts <corexploits@gmail.com>
2026-05-06 21:02:58 +0800

    Update shortcut.js

--- shortcut.js
+++ shortcut.js
@@ -6,7 +6,7 @@ const SHORTCUTS_DATA = [
     },
     {
         title: "Classrom G+",
-        url: "https://sites.google.com/view/drive-u-7-home/home?pli=1",
+        url: "https://i.ibb.co/CpDR7P5w/IMG-1070.png",
         image: "https://lh3.googleusercontent.com/sitesv/AA5AbUDnWos8vhxrOxU0fSC6i8orSwC2Te-LROYWUIV1xqoDSZI_FbdeHmO_MmIQrTzSyComMPGI5a5_1pLufI9niWfVECBB7ms79r8Lmf8dWk-BuMova0GX1-xJtJvGZ6AXQb84K8pxyrDCXUQxeH37LxheayUNQuoJFilYdUm3POjm9e3Hy2pFrqP8fFOC-aVA2ix52pnMrFwenLRs7iXGfPYdiwWPWPUmcPXV=w1280" // Example of a rectangle image
     }
 ];
