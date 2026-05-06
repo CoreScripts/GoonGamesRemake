a9f755f73c9cb6807d687b2ed189a897179c951d
CoreScripts <corexploits@gmail.com>
2026-05-06 21:11:01 +0800

    Update shortcut.js

--- shortcut.js
+++ shortcut.js
@@ -5,9 +5,9 @@ const SHORTCUTS_DATA = [
         image: "https://lh3.googleusercontent.com/Mxl8996vzcWWRU5RyKdTDA76GEPj9jQpuYc02nPgtwKgdqpWidM5YVqu8DHBd-D1sZ3sP1AOYB-DlNhMEpaLJxQcTQ=s128-rj-sc0x00ffffff"
     },
     {
-        title: "Classrom G+",
-        url: "https://i.ibb.co/CpDR7P5w/IMG-1070.png",
-        image: "https://lh3.googleusercontent.com/sitesv/AA5AbUDnWos8vhxrOxU0fSC6i8orSwC2Te-LROYWUIV1xqoDSZI_FbdeHmO_MmIQrTzSyComMPGI5a5_1pLufI9niWfVECBB7ms79r8Lmf8dWk-BuMova0GX1-xJtJvGZ6AXQb84K8pxyrDCXUQxeH37LxheayUNQuoJFilYdUm3POjm9e3Hy2pFrqP8fFOC-aVA2ix52pnMrFwenLRs7iXGfPYdiwWPWPUmcPXV=w1280" // Example of a rectangle image
+        title: "Classroom G+",
+        url: "https://sites.google.com/view/drive-u-7-home/home",
+        image: "https://i.ibb.co/CpDR7P5w/IMG-1070.png"
     }
 ];
 
