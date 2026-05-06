cd2bba470497840e912fa8cc34de55799aa0e32f
CoreScripts <corexploits@gmail.com>
2026-05-06 20:57:52 +0800

    Update shortcut.js

--- shortcut.js
+++ shortcut.js
@@ -1,14 +1,14 @@
-const proxy = (url) => `https://images.weserv.nl/?url=${encodeURIComponent(url)}&w=400&fit=inside&output=webp`;
-
 const SHORTCUTS_DATA = [
     {
         title: "Goon Games",
         url: "https://goon-games.vercel.app",
-        image: proxy("https://lh3.googleusercontent.com/Mxl8996vzcWWRU5RyKdTDA76GEPj9jQpuYc02nPgtwKgdqpWidM5YVqu8DHBd-D1sZ3sP1AOYB-DlNhMEpaLJxQcTQ=s128-rj-sc0x00ffffff)
+        image: "https://lh3.googleusercontent.com/Mxl8996vzcWWRU5RyKdTDA76GEPj9jQpuYc02nPgtwKgdqpWidM5YVqu8DHBd-D1sZ3sP1AOYB-DlNhMEpaLJxQcTQ=s128-rj-sc0x00ffffff"
     },
     {
         title: "Classrom G+",
         url: "https://sites.google.com/view/drive-u-7-home/home?pli=1",
-        image: proxy("https://lh3.googleusercontent.com/sitesv/AA5AbUDnWos8vhxrOxU0fSC6i8orSwC2Te-LROYWUIV1xqoDSZI_FbdeHmO_MmIQrTzSyComMPGI5a5_1pLufI9niWfVECBB7ms79r8Lmf8dWk-BuMova0GX1-xJtJvGZ6AXQb84K8pxyrDCXUQxeH37LxheayUNQuoJFilYdUm3POjm9e3Hy2pFrqP8fFOC-aVA2ix52pnMrFwenLRs7iXGfPYdiwWPWPUmcPXV=w1280")
+        image: "https://lh3.googleusercontent.com/sitesv/AA5AbUDnWos8vhxrOxU0fSC6i8orSwC2Te-LROYWUIV1xqoDSZI_FbdeHmO_MmIQrTzSyComMPGI5a5_1pLufI9niWfVECBB7ms79r8Lmf8dWk-BuMova0GX1-xJtJvGZ6AXQb84K8pxyrDCXUQxeH37LxheayUNQuoJFilYdUm3POjm9e3Hy2pFrqP8fFOC-aVA2ix52pnMrFwenLRs7iXGfPYdiwWPWPUmcPXV=w1280" // Example of a rectangle image
     }
 ];
+
+
