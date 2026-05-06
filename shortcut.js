aeb77e4397b9e02cb835852c7468e0f251361f21
CoreScripts <corexploits@gmail.com>
2026-05-06 17:01:44 +0800

    Create shortcut.js

---
+++ shortcut.js
@@ -0,0 +1,12 @@
+const SHORTCUTS_DATA = [
+    {
+        title: "Goon Games",
+        url: "https://goon-games.vercel.app",
+        image: "https://via.placeholder.com/150" // Replace with your image URL
+    },
+    {
+        title: "Drive U 7",
+        url: "https://sites.google.com/view/drive-u-7-home/home?pli=1",
+        image: "https://via.placeholder.com/300x150" // Example of a rectangle image
+    }
+];
