aa813ce13ed2021bb1c2df84176ecc6c2ab56503
CoreScripts <corexploits@gmail.com>
2026-05-17 17:40:23 +0800

    Update games.js

--- games.js
+++ games.js
@@ -97,7 +97,7 @@ const GAMES_DATA = [
         id: "12",
         title: "Shift At Midnight",
         image: proxy("https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/257204079/8c55402ce6c155cc8212644b158d3b4af95ab98a/movie_232x130.jpg?t=1758557357"),
-        url: "https://corescripts.github.io/ShiftAtMidnightPort/",
+        url: "games/midnightshift.html",
         mobile: true
     },
     {
@@ -111,7 +111,7 @@ const GAMES_DATA = [
         id: "14",
         title: "10 Minutes Till Dawn",
         image: proxy("https://img.itch.zone/aW1nLzg3MTU5NzMucG5n/original/0LqGxb.png"),
-        url: "https://10minutestilldawnonline.github.io/a7/10-minutes-till-dawn/",
+        url: "games/10mintilldawn.html",
         mobile: false
     },
     {
@@ -181,7 +181,7 @@ const GAMES_DATA = [
         id: "24",
         title: "Scrap Metal 3",
         image: proxy("https://www.drifted.com/wp-content/uploads/2018/10/scrap-metal-3-full-size.jpg"),
-        url: "https://scrapmetalgame.github.io/game/",
+        url: "game/scrapmetal3.html",
         mobile: true
     },
     {
@@ -202,162 +202,176 @@ const GAMES_DATA = [
         id: "27",
         title: "Drift Hunters",
         image: proxy("https://imgs.crazygames.com/games/drift-hunters/cover-1656950639575.png?metadata=none&quality=100&width=1200&height=630&fit=crop"),
-        url: "games/loader.html?url=https://pizza226.github.io/g5/class-447/",
+        url: "games/drifthunters.html",
+        mobile: true
     },
     {
         id: "28",
         title: "Bergentruck",
         image: proxy("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlE59KoYYECg_BnYUzsj63BtgbjW-eBmTsWQ&s"),
-        url: "games/loader.html?url=https://wanocapy.github.io/ChickenKingsVault/gamefiles/BERGENTRUCK.html"
+        url: "games/bergentruck.html",
+        mobile: true
     },
     {
         id: "29",
         title: "Brawl Stars",
         image: proxy("https://egw.news/_next/image?url=https%3A%2F%2Fegw.news%2Fuploads%2Fnews%2F1%2F17%2F1749024516847_1749024516847.webp&w=1920&q=75"),
-        url: "games/loader.html?url=https://wanocapy.github.io/ChickenKingsVault/gamefiles/Brawl%20Stars.html"
+        url: "https://wanocapy.github.io/ChickenKingsVault/gamefiles/Brawl%20Stars.html",
+        mobile: false
     },
     {
         id: "30",
         title: "Slow roads",
         image: proxy("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv2x4e1hUAIyWCezJBi-9WD-rxSzcXuJ6-cg&s"),
-        url: "games/loader.html?url=https://hackingschoolwebsites.github.io/slowroads/"
+        url: "games/slowroads.html",
+        mobile: true
     },
     {
         id: "31",
         title: "Spaceflight Simulator",
         image: proxy("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzoeJmX4F5LkPh0BCf66mzZPU2cfGb4HQDTQ&s"),
-        url: "games/loader.html?url=spaceflightsim.html"
+        url: "games/spaceflightsim.html",
+        mobile: true
     },
     {
         id: "32",
         title: "Super Star Car",
         image: proxy("https://imgs.crazygames.com/super-star-car_16x9/20260318150415/super-star-car_16x9-cover?metadata=none&quality=100&width=1200&height=630&fit=crop"),
-        url: "games/loader.html?url=superstar.html"
+        url: "games/superstar.html",
+        mobile: true
     },
     {
         id: "33",
         title: "Survivor.io",
         image: proxy("https://static.wikia.nocookie.net/nickelodeon/images/5/54/Survivor-io.png/revision/latest/scale-to-width-down/1200?cb=20241228053110"),
-        url: "games/loader.html?url=https://wanocapy.github.io/ChickenKingsVault/gamefiles/Survivor.io.html"
+        url: "games/https://wanocapy.github.io/ChickenKingsVault/gamefiles/Survivor.io.html",
+        mobile: false
     },
     {
         id: "34",
-        title: "Plants vs Zombies 2 Gardenless (no mobile)",
-        image: proxy("https://cdn.webgamer.io/games/mine-blocks/mine-blocks.1280x.85pc.webp"),
-        url: "games/loader.html?url=pvzgardenless.html"
+        title: "Bad Piggies",
+        image: proxy("https"),
+        url: "games/badpiggies.html",
+        mobile: false
     },
     {
         id: "35",
         title: "Minecraft Case Simulator",
         image: proxy("https://playgama.com/cdn-cgi/imagedelivery/LN2S-4p3-GgZvEx3IPaKUA/9bddc924-526a-4e52-a117-051258ffcb00/enlarged"),
-        url: "games/loader.html?url=https://funny-school.github.io/m1/MinecraftCaseSimulator/"
+        url: "https://funny-school.github.io/m1/MinecraftCaseSimulator/",
+        mobile: false
     },
     {
         id: "36",
         title: "Checkout Frenzy",
         image: proxy("https://play-lh.googleusercontent.com/8EXSe5kOvzugIwjjHsivULU7BZI05ZCYk6Hq1wIoTq3iEQXh_Kw-cpAYBtkP6G-jNkA2yoYKpSZ7G6PM_FhklTg=w526-h296-rw"),
-        url: "games/loader.html?url=https://funny-school.github.io/c1/checkout-frenzy/"
+        url: "https://funny-school.github.io/c1/checkout-frenzy/"
     },
     {
         id: "37",
         title: "Hungry Lamu",
         image: proxy("https://preview.redd.it/theory-about-hungry-lamu-v0-wsl9odzdlehf1.jpeg?width=1080&crop=smart&auto=webp&s=1f81f9605e88a3259d3a1dcde1188d261f62b69c"),
-        url: "games/loader.html?url=https://funny-school.github.io/h1/hungrylamu/"
+        url: "https://funny-school.github.io/h1/hungrylamu/"
     },
     {
         id: "38",
         title: "Backrooms",
         image: proxy("https://static.wikia.nocookie.net/the-indie-horror-game-world/images/f/fe/Ujyuijk.jpg/revision/latest?cb=20240219113658"),
-        url: "games/loader.html?url=https://funny-school.github.io/b/backrooms/"
+        url: "games/backrooms.html",
+        mobile: true
     },
     {
         id: "39",
         title: "Short Life 2",
         image: proxy("https://d.pacogames.com/images/wp/short-life-2.jpg"),
-        url: "games/loader.html?url=https://funny-school.github.io/s1/shortlife2/"
+        url: "https://funny-school.github.io/s1/shortlife2/"
     },
     {
         id: "40",
         title: "Drift Boss",
         image: proxy("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROIPlULnN4TadhlZ_R1P-BurRkHs2p7xZOUQ&s"),
-        url: "games/loader.html?url=https://drift-boss.gitlab.io/file/"
+        url: "https://drift-boss.gitlab.io/file/"
     },
     {
         id: "41",
         title: "Snow rider",
         image: proxy("https://zapgames.io/data/image/game/snow-rider-3d.jpg"),
-        url: "games/loader.html?url=https://funny-school.github.io/s1/snowrider/"
+        url: "https://funny-school.github.io/s1/snowrider/"
     },
     {
         id: "42",
         title: "Do not take this cat home",
         image: proxy("https://static.tvtropes.org/pmwiki/pub/images/avoidthecat.png"),
-        url: "games/loader.html?url=https://funny-school.github.io/donottakethiscathome/"
+        url: "https://funny-school.github.io/donottakethiscathome/"
     },
     {
         id: "43",
         title: "Once Upon a Coma",
         image: proxy("https://i.kickstarter.com/assets/018/258/330/203e0e65214f740f7e8ba8ec15720c02_original.jpg?anim=false&fit=cover&gravity=auto&height=873&origin=ugc&q=92&v=1521290841&width=1552&sig=FmRXFzoxQ75IHmedgXZdcazXgA9qrtLz6fYzjJ5GPPY%3D"),
-        url: "games/loader.html?url=https://funny-school.github.io/o1/onceuponacoma/"
+        url: "https://funny-school.github.io/o1/onceuponacoma/"
     },
     {
         id: "44",
         title: "Monkey Mart",
         image: proxy("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy80bhTRNp0dz0_N19K0E6XMwxaAvKMHUNbQ&s"),
-        url: "games/loader.html?url=https://funny-school.github.io/m1/monkeymart/"
+        url: "https://funny-school.github.io/m1/monkeymart/"
     },
     {
         id: "45",
         title: "Brain Test",
         image: proxy("https://i.ytimg.com/vi/ls8w1fvNwW8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAmWZ435EuS_HCHNRUta-nzBUQDZQ"),
-        url: "games/loader.html?url=https://funny-school.github.io/b/braintest/"
+        url: "https://funny-school.github.io/b/braintest/"
     },
     {
         id: "46",
         title: "Plague inc.",
         image: proxy("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQX3kjb_5CElkijzwbOb2xtmlWMZpxIRbzpHA&s"),
-        url: "games/loader.html?url=https://wanocapy.github.io/ChickenKingsVault/gamefiles/Plague%20Inc.html"
+        url: "https://wanocapy.github.io/ChickenKingsVault/gamefiles/Plague%20Inc.html"
     },
     {
         id: "47",
-        title: "Plants vs Zombies 2 Gardenless (Mobile support but lags)",
+        title: "Plants vs Zombies 2 Gardenless",
         image: proxy("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0MAGGPuzLWVOtp3SsuctbFYDQICiwcDcmvQ&s"),
-        url: "games/loader.html?url=https://corescripts.github.io/pvzge_web/"
+        url: "https://corescripts.github.io/pvzge_web/"
     },
     {
         id: "48",
         title: "Five Nights at Freddy's",
         image: proxy("https://static.wikia.nocookie.net/freddy-fazbears-pizza/images/d/da/FNaF1.jpg/revision/latest?cb=20230410020858"),
-        url: "games/loader.html?url=fnaf1.html"
+        url: "games/fnaf1.html",
+        mobile: true
     },
     {
         id: "49",
         title: "Indian Truck Simulator",
         image: proxy("https://m.media-amazon.com/images/I/91LiSen0kuL.jpg"),
-        url: "games/loader.html?url=https://ubgwtf.gitlab.io/indian-truck-simulator-3d/"
+        url: "games/indiantrucksim.html",
+        mobile: true
     },
     {
         id: "50",
         title: "Crazy Cars",
         image: proxy("https://img.poki-cdn.com/cdn-cgi/image/q=78,scq=50,width=1200,height=1200,fit=cover,f=png/2bc8004fcbb4387908a636e2a9d9ab4a/crazy-cars-logo.png"),
-        url: "games/loader.html?url=https://funny-school.github.io/c2/crazycars/"
+        url: "https://funny-school.github.io/c2/crazycars/"
     },
     {
         id: "51",
         title: "Tag",
         image: proxy("https://lh3.googleusercontent.com/aGEIWjyHtcDft29MvsLEOWThsfCDntUilOMdBX7I79KBlXkCxiQigFzyw5GlNocGLVNRkxic9_EptOonX3UtXO-hy0I=s1280-w1280-h800"),
-        url: "games/loader.html?url=https://funny-school.github.io/t1/tag/"
+        url: "https://funny-school.github.io/t1/tag/"
     },
     {
         id: "52",
         title: "Trees Hate You",
         image: proxy("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHG43IB8WzHpB0A7jZdUZKK5ys16I-L36lEw&s"),
-        url: "games/loader.html?url=treeshateyou.html"
+        url: "games/treeshateyou.html",
+        mobile: true
     },
     {
         id: "53",
         title: "Friday Night Funkin",
         image: proxy("https://friday-night-funkin-the-funkin-crew-inc.en.aptoide.com/_next/image?url=https%3A%2F%2Fcdn.aptoide.com%2Fimgs%2F7%2F6%2F9%2F769f9db715608f22e9174283d0f050e0_fgraphic.jpg&w=3840&q=60"),
-        url: "games/loader.html?url=fnf.html"
+        url: "games/fnaf1.html",
+        mobile: true
     }
 ];
