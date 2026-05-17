8f32bd814c1afc536c9658bf9d93bcb94088f7df
CoreScripts <corexploits@gmail.com>
2026-05-17 14:27:50 +0800

    Update games.js

--- games.js
+++ games.js
@@ -79,7 +79,7 @@ const GAMES_DATA = [
         id: "11",
         title: "Apes VS Helium",
         image: proxy("https://i.ytimg.com/vi/tCEGJgH4Nhc/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCyF9-K4-FBaWXDJ4zs12l1w_4CXA"),
-        url: "games/loader.html?url=Apes vs Helium.html"
+        url: "games/loader.html?url=apes-vs-helium/index.html"
     },
     {
         id: "12",
